50e98dd398ea25a6688d02ab52169350305d9966
maadt <147260993+maadt@users.noreply.github.com>
2024-02-19 19:47:17 +0900

    kadai1修正

--- Kadai1.java
+++ Kadai1.java
@@ -1,21 +1,17 @@
-class Kadai1 {
+public class Kadai1 {
     public static void main(String[] args) {
     String str = "abcadefg";
     System.out.println(isUniqueChars(str));
     }
 
     public static boolean isUniqueChars(String str) {
-        char[] charArray = str.toCharArray();
-        char charA;
-        char charB;
-        for (int i = 0; i < charArray.length - 1; i++) {//8
-            charA = charArray[i];
-            for (int j = i + 1; j < charArray.length; j++) {
-                charB = charArray[j];
-                if (charA == charB) {
-                    return false;
-                }
-            }
+        if (str.length() > 128) return false;
+        boolean[] char_set = new boolean[128];
+
+        for (int i = 0; i < str.length(); i++) {
+            int val = str.charAt(i);
+            if (char_set[val]) return false;
+            char_set[val] = true;
         }
         return true;
     }
