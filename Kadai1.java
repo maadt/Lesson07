8422b7b1ab48341aad3e27f8d396969bc7b87ac5
maadt <147260993+maadt@users.noreply.github.com>
2024-02-20 19:47:14 +0900

    kadai1解説追加

--- Kadai1.java
+++ Kadai1.java
@@ -9,9 +9,13 @@ public class Kadai1 {
         boolean[] char_set = new boolean[128];
 
         for (int i = 0; i < str.length(); i++) {
-            int val = str.charAt(i);
+            int val = str.charAt(i);//char型の値をint型の変数に代入する場合、自動的にchar型の値をその整数値に変換する。
+            //val：文字コードが格納される変数
+            //str：文字列を定義した変数
+            //charAt(i)：指定されたインデックスに位置する文字を文字コード(int型)で取得する
             if (char_set[val]) return false;
-            char_set[val] = true;
+            char_set[val] = true;//boolean型の配列の要素(特定の文字列)をtrueにする
+            //char_set[val]：特定の文字コード
         }
         return true;
     }
