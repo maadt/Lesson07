48a5e4ef05d76aa5edafd2d73170417bf828040c
maadt <147260993+maadt@users.noreply.github.com>
2024-02-27 15:46:29 +0900

    kadai2修正

--- kadai2.java
+++ kadai2.java
@@ -1,36 +1,28 @@
-import java.util.HashMap;
-import java.util.Map;
-
 public class Kadai2 {
     public static void main(String[] args) {
         String s = "dog";
-        String t = "god";
+        String t = "g o d";
         System.out.print(permutation(s, t));
     }
 
     public static boolean permutation(String s, String t) {
-        Map<Character, Integer> char_set = new HashMap<>();
 
-        if (s.length() != t.length()) {
-            return false;
+        if (s.length() != t.length()) return false;
+
+        int[] letters = new int[128];//文字列を判別するための配列を作成
+        char[] s_array = s.toCharArray();
+        //toCharArray()：文字列をchar型の配列に変換する
+
+        for (char c : s_array) {
+            letters[c]++; //文字列sのカウント
         }
-
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            char_set.put(c, char_set.getOrDefault(c, 0) + 1);
-        }
-
-        for (int j = 0; j < t.length(); j++) {
-            char c = t.charAt(j);
-            char_set.put(c, char_set.getOrDefault(c, 0) - 1);
-        }
-
-        for (Integer count : char_set.values()) {
-            if (count != 0) {
+        for (int i = 0; i < t.length(); i++) {
+            int c = (int)t.charAt(i);//ASCIIコードの値をint型にキャスト
+            if(--letters[c] < 0) {
+            //--letters[c]：letters配列の要素[c]をデクリメントしている
                 return false;
             }
         }
-
         return true;
     }
 }
